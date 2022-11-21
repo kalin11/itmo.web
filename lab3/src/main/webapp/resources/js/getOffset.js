42b450f508191355c9501f8811a4a7a0d33b7ca5
kalin11 <336805@niuitmo.ru>
2022-11-21 13:10:24 +0300

    deleted
    deleted

--- lab3/src/main/webapp/resources/js/getOffset.js
+++
@@ -1,4 +0,0 @@
-function getOffset(){
-    document.getElementById("date").value = new Date().getTimezoneOffset()/(-60);
-    document.getElementById("mainForm:offset").value = new Date().getTimezoneOffset()/(-60);
-}
